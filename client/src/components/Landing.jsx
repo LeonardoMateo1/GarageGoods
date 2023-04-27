b219d88c56d48ba6692d44e5ae6ec22bd82bfc91
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-04-27 16:51:49 -0400

    Basic frontend setup / cleanup

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div>
+            <h1 className='bg-red-500 text-6xl'>Hello</h1>
+        </div>
+    )
+}
+
+export default Landing
