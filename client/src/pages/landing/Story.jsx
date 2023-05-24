a26265819b82bb985fe2196aa35a35464ac80e43
Leonardo Mateo <leom305131@gmail.com>
2023-05-24 18:11:05 -0400

    put text there as placeholder still need to revise

--- client/src/pages/landing/Story.jsx
+++ client/src/pages/landing/Story.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Story = () => {
   return (
-    <div>Story</div>
+    <div>
+        <div className='w-full h-[80vh] flex items-center justify-center'>
+            <div className='w-[80%] h-full flex flex-col items-center text-center justify-center'>
+                <h3 className='mb-10 text-4xl font-bold'>Our Story</h3>
+                <p className='text-md'>
+                    At its core, "GarageGoods is more than just a success story of an online marketplace. 
+                    It is a testament to human connection, the value of second chances, and the transformative power of a community-focused approach in the digital age. 
+                    By the end of this captivating journey, readers gain an insight into how GarageGoods became an integral part of its users' lives, 
+                    making a significant socio-economic impact and paving the way for the future of sustainable e-commerce."
+                </p>
+            </div>
+        </div>
+    </div>
   )
 }
 
