f2cb434d7a2d3fc37abca14c7aa5b534231a3aa3
Leonardo Mateo <leom305131@gmail.com>
2023-05-24 17:19:53 -0400

    fixed xtra div

--- client/src/pages/landing/Testimonial.jsx
+++ client/src/pages/landing/Testimonial.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Testimonial = () => {
   return (
     <div>
-        <div className='w-full h-screen bg-gradient-to-r from-action to-yellow-600'></div>
+        <div className='w-full h-screen bg-gradient-to-r from-action to-yellow-600'>
 
         </div>
     </div>
