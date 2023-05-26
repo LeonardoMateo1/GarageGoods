147b6518505f24861c5caeba5f55cf8487ba7c52
Leonardo Mateo <leom305131@gmail.com>
2023-05-26 01:33:40 -0400

    Started Log Form page / boiler plate code

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Nav from '../Nav'
 
 const Login = () => {
   return (
     <div>
         <Nav/>
+        <div className='flex items-center justify-center w-full h-screen'>
+            <div className='w-[20rem] border rounded-xl h-[30rem] flex flex-col items-center justify-center shadow-md'>
+                <h2>Welcome Back</h2>
+                <p>New to GarageGoods <Link to='/SignUp' className='font-bold text-blue-500'>Sign Up for Free</Link></p>
+                <input type="text" name='email' className='' />
+                <input type="text" name='password' className='' />
+                <input type="submit" value="Login" />
+            </div>
+        </div>
     </div>
   )
 }
