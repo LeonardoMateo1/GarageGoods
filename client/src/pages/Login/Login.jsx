d7cd1ab4f7dcd6842f5c75814722e3bd7e91a36a
Leonardo Mateo <leom305131@gmail.com>
2023-05-26 02:04:01 -0400

    Added styling to Login Form page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Nav from '../Nav'
+import SVG1 from '../../assets/Login_illustration.png'
 
 const Login = () => {
 
@@ -35,12 +36,21 @@ const Login = () => {
     <div>
         <Nav/>
         <div className='flex items-center justify-center w-full h-screen'>
-            <form onSubmit={handleLogin} className='w-[20rem] border rounded-xl h-[30rem] flex flex-col items-center justify-center shadow-md'>
-                <h2>Welcome Back</h2>
-                <p>New to GarageGoods <Link to='/SignUp' className='font-bold text-blue-500'>Sign Up for Free</Link></p>
-                <input type="text" onChange={handleLoginInputs} name='email' className='border' />
-                <input type="password" onChange={handleLoginInputs} name='password' className='border' />
-                <input type="submit" value="Login" />
+            <form onSubmit={handleLogin} className='w-[20rem] border rounded-xl h-[35rem] flex items-center justify-center shadow-md'>
+              <div className='flex-col flex gap-5 w-[80%] h-[85%] items-center justify-between'>
+                <div className='flex flex-col items-center justify-center'>
+                  <h2 className='text-3xl font-semibold'>Log In</h2>
+                  <img src={SVG1} alt="signUp" className='object-cover w-full h-[15rem]' />
+                </div>
+                <div className='flex flex-col items-center justify-center w-full gap-4'>
+                  <p className='text-xs'>New to GarageGoods <Link to='/SignUp' className='font-bold text-blue-500'>Sign Up for Free</Link></p>
+                  <div className='flex flex-col items-center justify-center w-full gap-3'>
+                    <input type="text" onChange={handleLoginInputs} name='email' placeholder='Email' className='py-2 pl-3 pr-10 border rounded-2xl' />
+                    <input type="password" onChange={handleLoginInputs} name='password' placeholder='Password' className='py-2 pl-3 pr-10 border rounded-2xl' />
+                  </div>
+                  <input type="submit" value="Login"  className='w-full py-2 rounded-md shadow-md bg-action'/>
+                </div>
+              </div>
             </form>
         </div>
     </div>
