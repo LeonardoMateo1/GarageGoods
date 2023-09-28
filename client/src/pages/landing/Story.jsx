fd0d3696330077bfc98aefb3985885a381ca4314
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-09-27 23:31:35 -0400

    added image to story as well as fixed max-width

--- client/src/pages/landing/Story.jsx
+++ client/src/pages/landing/Story.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import marketplace from '../../assets/marketplace_image.jpg';
 
 const Story = () => {
   return (
     <div>
-        <div className='w-full h-[80vh] flex items-center justify-center'>
-            <div className='w-[80%] h-full flex flex-col items-center text-center justify-center'>
+        <div className='w-full h-[80vh] flex items-center justify-center max-width px-4 flex-col'>
+            <div className='flex flex-col items-center justify-center w-full mb-[7rem] text-center'>
                 <h3 className='mb-10 text-4xl font-bold'>Our Story</h3>
-                <p className='text-md'>
+                <p className='font-semibold text-md'>
                     At its core, "GarageGoods is more than just a success story of an online marketplace. 
                     It is a testament to human connection, the value of second chances, and the transformative power of a community-focused approach in the digital age. 
                     By the end of this captivating journey, readers gain an insight into how GarageGoods became an integral part of its users' lives, 
                     making a significant socio-economic impact and paving the way for the future of sustainable e-commerce."
                 </p>
             </div>
+            <img src={marketplace} alt="marketplace" className='w-full h-[30rem] object-cover object-bottom' />
         </div>
     </div>
   )
