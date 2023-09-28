2cffc0ec9b1925d8851a92849ddf72b2e96818b3
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-09-27 22:51:46 -0400

    Fixed depreciated dependencies and landing css

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -20,19 +20,21 @@ const Landing = () => {
                         <Link to='/SignUp' className='px-5 py-2 text-2xl font-bold text-white rounded-lg bg-action'><button>Start Here</button></Link>
                     </div>
                 </div>
-                <div>
+                <div className='relative'>
                     <img src={smile} alt="" className='w-full h-[25rem] object-contain rounded-full bg-action' />
-                    <div className='absolute right-[8rem] top-[12rem] z-10 bg-white flex flex-col items-center p-2 border rounded-md shadow-sm justify-normal'>
-                        <p className='text-lg font-bold'>15k</p>
-                        <p className='font-medium text-light-text '>Happy Customers</p>
-                    </div>
-                    <div className='absolute right-[8rem] top-[20rem] z-10 bg-white flex flex-col items-center p-2 border rounded-md shadow-sm justify-normal'>
-                        <p className='text-lg font-bold'>+ 7.5k</p>
-                        <p className='font-medium text-light-text '>Products Sold</p>
-                    </div>
-                    <div className='absolute right-[8rem] top-[28rem] z-10 bg-white flex flex-col items-center p-2 border rounded-md shadow-sm justify-normal'>
-                        <p className='text-lg font-bold'>+ 30k</p>
-                        <p className='font-medium text-light-text '>Members</p>
+                    <div className='absolute z-10 w-full bg-red-500 top-[-12rem] right-[-9rem]'>
+                        <div className='absolute right-[8rem] top-[12rem] z-10 bg-white flex flex-col items-center p-2 border rounded-md shadow-sm justify-normal'>
+                            <p className='text-lg font-bold'>15k</p>
+                            <p className='font-medium text-light-text '>Happy Customers</p>
+                        </div>
+                        <div className='absolute right-[8rem] top-[20rem] z-10 bg-white flex flex-col items-center p-2 border rounded-md shadow-sm justify-normal'>
+                            <p className='text-lg font-bold'>+ 7.5k</p>
+                            <p className='font-medium text-light-text '>Products Sold</p>
+                        </div>
+                        <div className='absolute right-[8rem] top-[28rem] z-10 bg-white flex flex-col items-center p-2 border rounded-md shadow-sm justify-normal'>
+                            <p className='text-lg font-bold'>+ 30k</p>
+                            <p className='font-medium text-light-text '>Members</p>
+                        </div>
                     </div>
                 </div>
             </div>
